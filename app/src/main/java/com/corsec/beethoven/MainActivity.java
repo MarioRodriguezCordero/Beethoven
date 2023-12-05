4300c4bc7ec4f714e7c3eb7578e52a5b6cda6b4a
Mario <mario.rodcor@educa.jcyl.es>
2023-12-05 11:08:57 +0100

    1º commit

--- app/src/main/java/com/corsec/beethoven/MainActivity.java
+++ app/src/main/java/com/corsec/beethoven/MainActivity.java
@@ -2,29 +2,26 @@ package com.corsec.beethoven;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.tb1);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menutoolbar, menu);
-        return true;
+    public void inicioSesion() {
+        Intent intent = new Intent(this, inicioSesion.class);
+        startActivity(intent);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        return super.onOptionsItemSelected(item);
+    public void registrarse() {
+        Intent intent = new Intent(this, Registrarse.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/corsec/beethoven/inicioSesion.java
@@ -0,0 +1,15 @@
+package com.corsec.beethoven;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class inicioSesion extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_sesion);
+    }
+
+
+}
